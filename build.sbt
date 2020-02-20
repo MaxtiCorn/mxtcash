4a16bf89e788aee1c501bef40c82a2155077ac1d
maxticorn <letunovskeymax@gmail.com>
2020-02-20 19:11:29 +0500

    monix -> zio

--- build.sbt
+++ build.sbt
@@ -7,5 +7,6 @@ enablePlugins(JavaAppPackaging)
 libraryDependencies ++= Seq(
   "org.http4s" %% "http4s-blaze-server" % "0.21.1",
   "org.http4s" %% "http4s-dsl" % "0.21.1",
-  "io.monix" %% "monix" % "3.1.0"
+  "dev.zio" %% "zio" % "1.0.0-RC17",
+  "dev.zio" %% "zio-interop-cats" % "2.0.0.0-RC10"
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,31 +1,54 @@
-import cats.effect.{ExitCode, Resource}
-import monix.eval.{Task, TaskApp}
+import cats.{Applicative, Defer}
+import cats.effect.{ConcurrentEffect, ExitCode, Timer}
+import cats.effect.ExitCode._
+import zio.{RIO, ZIO, ZEnv, system}
+import zio.interop.catz._
+import fs2.Stream
 import org.http4s.HttpRoutes
 import org.http4s.dsl._
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
-import scala.util.Try
 
-object Main extends TaskApp {
-  override def run(args: List[String]): Task[ExitCode] = {
-    val dsl = Http4sDsl[Task]
+object Main extends zio.App {
+  type AppTask[A] = RIO[ZEnv, A]
+
+  def routes[F[_]: Applicative: Defer](dsl: Http4sDsl[F]): HttpRoutes[F] = {
     import dsl._
 
-    val routes = HttpRoutes.of[Task] {
+    HttpRoutes.of[F] {
       case GET -> Root / "hello" / name =>
         Ok(s"hello, $name")
     }
+  }
 
+  def httpServe[F[_]: ConcurrentEffect: Timer](
+      host: String,
+      port: Int,
+      routes: HttpRoutes[F]
+  ): Stream[F, ExitCode] =
+    BlazeServerBuilder[F]
+      .bindHttp(port, host)
+      .withHttpApp(routes.orNotFound)
+      .serve
+
+  def port(): ZIO[system.System, Throwable, Int] =
     (for {
-      port <- Resource.liftF(
-        Task.fromTry(Try(System.getProperty("http.port").toInt))
-      )
-      _ <- BlazeServerBuilder[Task]
-        .bindHttp(port, "0.0.0.0")
-        .withHttpApp(routes.orNotFound)
-        .resource
-    } yield ()).use { _ =>
-      Task.never
+      property <- system.property("http.port")
+      port = property.flatMap(_.toIntOption)
+    } yield port) someOrFail new RuntimeException(
+      "couldn't read http.port property"
+    )
+
+  override def run(args: List[String]): ZIO[ZEnv, Nothing, Int] = {
+    ZIO.runtime[ZEnv].flatMap { implicit rt =>
+      (for {
+        port <- Stream.eval(port())
+        _ <- httpServe("0.0.0.0", port, routes(Http4sDsl[AppTask]))
+      } yield ()).compile.drain
+        .foldM(
+          th => ZIO.die(th).as(Error.code),
+          _ => ZIO.succeed(Success.code)
+        )
     }
   }
 }
